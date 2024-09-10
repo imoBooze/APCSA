4d98c6314617e5d59575ff27bd13034ac61938fa
imoBooze <31322597+imoBooze@users.noreply.github.com>
2024-09-10 12:30:12 -0700

    Add files via upload

---
+++ problemSet1A/Slandertron_2000.java
@@ -0,0 +1,16 @@
+/*
+ * ARRRRRRGH
+ */
+
+import java.util.Scanner;
+
+class Slandertron_2000
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = input.nextLine();
+		System.out.print( name + "'s mother is a hamster and their father smells of elderberries.");
+		input.close();
+	}
+}
