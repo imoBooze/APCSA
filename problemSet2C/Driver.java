ed71a1ff803f4d5374c9c128c28dc7cd7ca36bd0
imoBooze <31322597+imoBooze@users.noreply.github.com>
2024-09-29 09:15:23 -0700

    Completion of the user friendlification

--- problemSet2C/Driver.java
+++ problemSet2C/Driver.java
@@ -44,39 +44,71 @@ class Driver {
       OrderedPair op1 = new OrderedPair(Bx, By);
 
       System.out.println(
-          "A line between these two coordinates has a slope of "
+          "A line between coordinates "
+              + op0
+              + " and "
+              + op1
+              + " has a slope of "
               + Formula.findSlope(op0, op1)
               + ".");
 
     } else if (option == 2) {
+      System.out.print("Input the x value of the first coordinate: ");
       double Ax = input.nextDouble();
+
+      System.out.print("Input the y value of the first coordinate: ");
       double Ay = input.nextDouble();
+
+      System.out.print("Input the x value of the second coordinate: ");
       double Bx = input.nextDouble();
+
+      System.out.print("Input the y value of the second coordinate: ");
       double By = input.nextDouble();
       input.nextLine();
 
       OrderedPair op0 = new OrderedPair(Ax, Ay);
       OrderedPair op1 = new OrderedPair(Bx, By);
 
-      System.out.println(Formula.findMidpoint(op0, op1));
+      System.out.println("The midpoint of the coordinates "
+          + op0
+          + " and "
+          + op1
+          + " is "
+          + Formula.findMidpoint(op0, op1)
+          + ".");
 
     } else if (option == 3) {
+      System.out.print("Input the initial value of the arithmetic series: ");
       double init = input.nextDouble();
+
+      System.out.print("Input the difference between each value: ");
       double diff = input.nextDouble();
+
+      System.out.print("Input the number of terms: ");
       int terms = input.nextInt();
       input.nextLine();
 
-      System.out.println(Formula.findArithmeticSeriesSum(init, diff, terms));
+      System.out.println("The sum of this arithmetic series is "
+          + Formula.findArithmeticSeriesSum(init, diff, terms)
+          + ".");
 
     } else if (option == 4) {
+      System.out.print("Input the initial value of the geometric series: ");
       double init = input.nextDouble();
+
+      System.out.print("Input the ratio between each value: ");
       double ratio = input.nextDouble();
+
+      System.out.print("Input the number of terms: ");
       int terms = input.nextInt();
       input.nextLine();
 
-      System.out.println(Formula.findGeometricSeriesSum(init, ratio, terms));
+      System.out.println("The sum of this geometric series is: "
+          + Formula.findGeometricSeriesSum(init, ratio, terms)
+          + ".");
 
     } else if (option == 5) {
+      System.out.print("Input the number of sides on the die: ");
       int sides = input.nextInt();
       input.nextLine();
 
