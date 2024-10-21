23bfa35313d6cb1b7486242fa44f0631e67873ab
imoBooze <31322597+imoBooze@users.noreply.github.com>
2024-10-21 13:26:49 -0700

    Finish (hopefully)

--- problemSet3C/Elby.java
+++ problemSet3C/Elby.java
@@ -28,6 +28,16 @@ public class Elby {
         || findKeyword(statement, "sister", 0) >= 0
         || findKeyword(statement, "brother", 0) >= 0) {
       response = "Tell me more about your family.";
+    } else if (findKeyword(statement, "you", 0) >= 0
+        && findKeyword(statement, "me", statement.indexOf("you") + "you".length()) >= 0) {
+      response = transformYouMeStatement(statement);
+    } else if (findKeyword(statement, "I", 0) >= 0
+        && findKeyword(statement, "you", statement.indexOf("I") + "I".length()) >= 0) {
+      response = transformIMeStatement(statement);
+    } else if (findKeyword(statement, "I want to", 0) >= 0) {
+      response = transformIWantToStatement(statement);
+    } else if (findKeyword(statement, "I want", 0) >= 0) {
+      response = transformIWantStatement(statement);
     } else {
       response = getRandomResponse();
     }
@@ -104,5 +114,86 @@ public class Elby {
       goalPos = phrase.indexOf(goal, goalPos + 1);
     }
     return -1;
+  }
+
+  /**
+   * Take a statement with "you <something> me" and transform it into "What makes you think that I
+   * <something> you?"
+   *
+   * @param statement the user statement, assumed to contain "you" followed by "me"
+   * @return the transformed statement
+   */
+  public String transformYouMeStatement(String statement) {
+    // Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals(".")) {
+      statement = statement.substring(0, statement.length() - 1);
+    }
+
+    int posOfYou = findKeyword(statement, "you", 0);
+    int posOfMe = findKeyword(statement, "me", posOfYou + "you".length());
+
+    String restOfStatement = statement.substring(posOfYou + "you".length(), posOfMe).trim();
+    return "What makes you think that I " + restOfStatement + " you?";
+  }
+
+  /**
+   * Take a statement with "I want to <something>." and transform it into "What would it mean to
+   * <something>?"
+   *
+   * @param statement the user statement, assumed to contain "I want to"
+   * @return the transformed statement
+   */
+  public String transformIWantToStatement(String statement) {
+    // Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals(".")) {
+      statement = statement.substring(0, statement.length() - 1);
+    }
+    int pos = findKeyword(statement, "I want to", 0);
+    String restOfStatement = statement.substring(pos + 9).trim();
+    return "What would it mean to " + restOfStatement + "?";
+  }
+
+  /**
+   * Take a statement with "I want <something>." and transform it into Would you really be happy if
+   * you had <something>?
+   *
+   * @param statement the user statement, assumed to contain "I want"
+   * @return the transformed statement
+   */
+  public String transformIWantStatement(String statement) {
+    // Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals(".")) {
+      statement = statement.substring(0, statement.length() - 1);
+    }
+    int pos = findKeyword(statement, "I want", 0);
+    String restOfStatement = statement.substring(pos + "I want".length()).trim();
+    return "Would you really be happy if you had " + restOfStatement + "?";
+  }
+
+  /**
+   * Take a statement with "I <something> you" and transform it into "Why do you <something> me?"
+   *
+   * @param statement the user statement, assumed to contain "I" followed by something "you"
+   * @return the transformed statement
+   */
+  public String transformIMeStatement(String statement) {
+    // Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals(".")) {
+      statement = statement.substring(0, statement.length() - 1);
+    }
+
+    int posOfI = findKeyword(statement, "I", 0);
+    int posOfYou = findKeyword(statement, "you", posOfI + "I".length());
+
+    String restOfStatement = statement.substring(posOfI + "I".length(), posOfYou).trim();
+    return "Why do you " + restOfStatement + " me?";
   }
 }
