7e52f0ecd2ea5789c904882a3b4a7fc2c91eab6a
imoBooze <31322597+imoBooze@users.noreply.github.com>
2024-10-20 23:00:42 -0700

    Test findKeyword

--- problemSet3C/Elby.java
+++ problemSet3C/Elby.java
@@ -17,7 +17,7 @@ public class Elby {
   public String getResponse(String statement) {
     String response = "";
 
-    if (statement.indexOf("no") >= 0) {
+    if (findKeyword(statement, "no", 0) >= 0) {
       response = "Why so negative?";
 
     } else if (statement.indexOf("dog") >= 0 || statement.indexOf("cat") >= 0) {
@@ -62,5 +62,48 @@ public class Elby {
 
     return response;
   }
-}
 
+  /**
+   * Search for one word in phrase. The search is not case sensitive. This method will check that
+   * the given goal is not a substring of a longer string (so, for example, "I know" does not
+   * contain "no").
+   *
+   * @param statement the string to search
+   * @param goal the string to search for
+   * @param startPos the character of the string to begin the search at
+   * @return the index of the first occurrence of goal in statement or -1 if it's not found
+   */
+  public int findKeyword(String statement, String goal, int startPos) {
+    String phrase = statement.trim().toLowerCase();
+    goal = goal.toLowerCase();
+
+    // The only change to incorporate the startPos is in the line below
+    int goalPos = phrase.indexOf(goal, startPos);
+
+    // Refinement--make sure the goal isn't part of a word
+    while (goalPos >= 0) {
+      // Find the string of length 1 before and after the word
+      String before = " ", after = " ";
+
+      if (goalPos > 0) {
+        before = phrase.substring(goalPos - 1, goalPos);
+      }
+
+      if (goalPos + goal.length() < phrase.length()) {
+        after = phrase.substring(goalPos + goal.length(), goalPos + goal.length() + 1);
+      }
+
+      /* determine the values of goalPos, before, and after at this point */
+
+      // If before and after aren't letters, we've found the word
+      if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0))
+          && ((after.compareTo("a") < 0) || (after.compareTo("z") > 0))) {
+        return goalPos;
+      }
+
+      // The last position didn't work, so let's find the next, if there is one.
+      goalPos = phrase.indexOf(goal, goalPos + 1);
+    }
+    return -1;
+  }
+}
