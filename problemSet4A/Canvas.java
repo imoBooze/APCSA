5bf425c176374b7402835c3ebdb565c18aa9b8d9
imoBooze <31322597+imoBooze@users.noreply.github.com>
2024-10-26 23:39:56 -0700

    Finish Sierpinski triangle

--- problemSet4A/Canvas.java
+++ problemSet4A/Canvas.java
@@ -1,10 +1,11 @@
 class Canvas {
   static int width = 1500;
   static int height = 1500;
+
   public static void main(String[] args) {
     StdDraw.setCanvasSize(width, height);
     StdDraw.setScale(0, 1500);
-    circles();
+    sierpinski(width - 300, height - 300, 500);
   }
 
   static void grid(int spacing) {
@@ -28,9 +29,39 @@ class Canvas {
     }
   }
 
-  static void sierpinski() {
-    OrderedPair op0 = new OrderedPair();
-    for (int i = 0; i < 10; i++) {
+  //x and y for where to place the triangle from the leftmost point, w for vertex length
+  static void sierpinski(double x, double y, double w) {
+    OrderedPair op0 = new OrderedPair(x, y); // Leftmost
+    OrderedPair op1 = new OrderedPair(w, y); // Rightmost
+    OrderedPair op2 =
+        new OrderedPair(
+            (x + w) / 2, y - Math.sqrt(Math.pow(w, 2) - Math.pow(w / 2, 2))); // Bottommost
+    StdDraw.setPenRadius(0.01);
+
+    // Plot down the three vertexes
+    StdDraw.point(op0.getX(), op0.getY());
+    StdDraw.point(op1.getX(), op1.getY());
+    StdDraw.point(op2.getX(), op2.getY());
+
+    // Pick leftmost vertex as initial (doesn't really matter which)
+    OrderedPair chosen = op0;
+
+    // Infinitely draw
+    while (true) {
+      int n = (int) (Math.random() * 3);
+
+      OrderedPair sel = new OrderedPair(); // Choose a random vertex
+
+      if (n == 0) {
+        sel = op0;
+      } else if (n == 1) {
+        sel = op1;
+      } else if (n == 2) {
+        sel = op2;
+      }
+
+      chosen = Formula.findMidpoint(sel, chosen);
+      StdDraw.point(chosen.getX(), chosen.getY()); // Draw!
     }
   }
 }
