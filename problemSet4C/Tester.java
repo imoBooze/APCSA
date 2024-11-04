f41af63eb541bc6009e7b67862c44b3b2b04c3e8
imoBooze <31322597+imoBooze@users.noreply.github.com>
2024-11-03 16:51:08 -0800

    AAAAAARRRRRRGGGGGHHH

---
+++ problemSet4C/Tester.java
@@ -0,0 +1,16 @@
+// Question 2
+
+class Tester {
+  public static void main(String[] args) {
+    // System.out.println(Review.sentimentVal("trash"));
+    // System.out.println(Review.sentimentVal("potato"));
+    // System.out.println(Review.sentimentVal("explode"));
+    // System.out.println(Review.sentimentVal("cheap"));
+    // System.out.println(Review.sentimentVal("expensive"));
+
+    // double num = sentimentVal("warm");
+    // String word = sentimentVal(0.5);
+    // System.out.println(Review.sentimentVal("123"));
+    // System.out.println(Review.sentimentVal("aaa", "bbb"));
+  }
+}
